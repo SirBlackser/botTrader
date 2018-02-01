8dd5d09045b67427c92d754e7e0605e121fc0abb
dries <dries@informadvies.be>
2018-02-01 22:05:19 +0100

    dit lijkt mij precies net iets te gemakkelijk. But I'm out off energie for today. We should go over it together.

--- src/main/java/be/ds/projects/botTrader/workshop/AlgorithmWorkshop.java
+++ src/main/java/be/ds/projects/botTrader/workshop/AlgorithmWorkshop.java
@@ -1,11 +1,36 @@
 package be.ds.projects.botTrader.workshop;
 
+import be.ds.projects.botTrader.model.Ask;
+import be.ds.projects.botTrader.model.Ticker;
+
+import java.util.LinkedList;
+
 /**
  * @author Steven de Cleene
  */
 public class AlgorithmWorkshop {
 
-    public void doAwesomeShit() {
+    private long moveingAverage;
+    private double moveingAverageNumber;
+    private double percentageProfit;
+    private double sellDropOff;
+    private LinkedList values;
+    private long sum;
+    private LinkedList<Ask> buys;
+    private double prevMoveingAverage;
+
+    public AlgorithmWorkshop(double moveingAverageNumber, double percentageProfit, double sellDropOff) {
+        this.moveingAverageNumber = moveingAverageNumber;
+        this.percentageProfit = 1.05 + (percentageProfit/100);
+        this.sellDropOff = sellDropOff;
+        this.moveingAverage = 0;
+        this.prevMoveingAverage = this.moveingAverage;
+        values = new LinkedList();
+        buys = new LinkedList<>();
+        sum = 0;
+    }
+
+    public void doAwesomeShitV1(Ticker ticker) {
         /*
             @TODO: Implement algorithm
             Scheduler werkt nu, in main kunt ge scheduler instellen (als dat niet duidelijk is hoe laat je maar weten)
@@ -34,6 +59,24 @@ public class AlgorithmWorkshop {
 
             enjoy ;)
          */
+
+        this.prevMoveingAverage = this.moveingAverage;
+        if (values.size() == moveingAverageNumber && moveingAverageNumber > 0)
+        {
+            sum -= ((Double) values.getFirst()).doubleValue();
+            values.removeFirst();
+        }
+        sum += ticker.getLast();
+        values.addLast(new Double(ticker.getLast()));
+        moveingAverage = sum / values.size();
+
+        if(buys.size() > 0 && buys.getLast().getPrice()*percentageProfit < ticker.getLast() && prevMoveingAverage > moveingAverage) {
+            // TODO sell
+        } else if(moveingAverage > prevMoveingAverage) {
+            // TODO buy
+        } else if((buys.getLast().getPrice()*(1-(sellDropOff/100))) < moveingAverage) {
+            // TODO sell
+        }
     }
 
 }
